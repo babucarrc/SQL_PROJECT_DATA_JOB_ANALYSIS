94e90cfc8f2aefb927146f7fcee3b68d2c033dba
babucarrc <babucarrc@gmail.com>
2025-07-18 06:50:24 -0700

    upload sql files

---
+++ project_sql/3._top_demand_skills.sql
@@ -0,0 +1,41 @@
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND job_work_from_home = True
+GROUP BY 
+    skills
+ORDER BY 
+    demand_count DESC
+LIMIT 5;
+
+---- This query retrieves the top 5 skills in demand for Data Analyst jobs that offer work from home.
+---- It counts the number of job postings for each skill and orders them by demand count.
+
+[
+  {
+    "skills": "sql",
+    "demand_count": "7291"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "4611"
+  },
+  {
+    "skills": "python",
+    "demand_count": "4330"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "3745"
+  },
+  {
+    "skills": "power bi",
+    "demand_count": "2609"
+  }
+]
